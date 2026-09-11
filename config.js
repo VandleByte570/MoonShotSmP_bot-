9c464164f814b14536dbbf3fd93cf1fd2681c9db
RISHABH SHAKYA <283314222+VandleByte570@users.noreply.github.com>
2026-09-11 21:48:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,18 @@ const token = process.env.DISCORD_TOKEN;
 const falixKey = process.env.FALIX_API_KEY;
 const serverId = process.env.FALIX_SERVER_ID;
 
+if (!token) {
+  throw new Error("DISCORD_TOKEN is missing in .env");
+}
+
+if (!falixKey) {
+  throw new Error("FALIX_API_KEY is missing in .env");
+}
+
+if (!serverId) {
+  throw new Error("FALIX_SERVER_ID is missing in .env");
+}
+
 module.exports = {
   token,
   falixKey,
